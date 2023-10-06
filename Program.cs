12f3e9a430471ec769f19cfb2ec27aef5bd48c90
Nico Macioszek <nico.macioszek@untitled.software>
2023-10-06 18:04:40 +0200

    Server will now shut down when restarter closes without 'lift' input.

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ internal static class Program
     private static void Main()
     {
         RestartWatcher watcher = new();
+        AppDomain.CurrentDomain.ProcessExit += new EventHandler((sender, e) => ProcessExit(sender, e, watcher));           
         
         watcher.Init();
         
@@ -13,7 +14,10 @@ internal static class Program
         do {
             cancelInput = Console.ReadLine() ?? "";                
         } while (cancelInput != "lift");
-        
+    }
+
+    private static void ProcessExit(object sender, EventArgs e, RestartWatcher watcher)
+    {
         watcher.StopAssettoServer();
     }
 }
